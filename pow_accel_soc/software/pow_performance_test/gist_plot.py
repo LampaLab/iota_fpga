2d191e303af5a8a0d5b034e0269d0840d257d065
Korotkyi Ievgen <KorotkiyEugene@gmail.com>
2018-07-15 17:55:44 +0300

    Update gist_plot.py

--- pow_accel_soc/software/pow_performance_test/gist_plot.py
+++ pow_accel_soc/software/pow_performance_test/gist_plot.py
@@ -6,6 +6,6 @@ d = pd.read_csv('pow_delays.csv')
 pow_delays = d['POWdelay']
 
 plt.hist(pow_delays, 100, facecolor='blue', alpha=0.5)
-plt.xlabel("POW delay, sec (for MWM = 14)")
+plt.xlabel("POW delay, sec")
 plt.title('Average POW delay: %f sec' % np.mean(pow_delays))
 plt.show()
